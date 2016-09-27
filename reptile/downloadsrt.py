e15b63a6bd164e0236f31659bd041c5f09f88933
AshyHarry <harryashy@sina.com>
2016-09-28 00:16:22 +0800

    1.get original json data from netease ruquest url 2.download all the srt in this course with a loop

--- reptile/downloadsrt.py
+++ reptile/downloadsrt.py
@@ -1,19 +1,35 @@
+# -*- coding: UTF-8 -*-
+import os
 import urllib2
 import json
 
-def download():
-    fp = open('orginaldata.txt','r')
-    url_json = json.load(fp)
-    url_srt_cn = url_json['data']['videoList'][2]['subList'][0]['subUrl']
-    url_srt_en = url_json['data']['videoList'][2]['subList'][1]['subUrl']
-    # url_srt_cn = url_json['data']['videoList'][3]['subList'][0]['subUrl']
-    # url_srt_en = url_json['data']['videoList'][3]['subList'][1]['subUrl']
-    print url_srt_cn,url_srt_en
-    f = urllib2.urlopen(url_srt_en)
-    srt_name = 'harry.srt'
-    with open(srt_name, "wb") as srt_cn:
-        srt_cn.write(f.read())
 
+def download_srt():
+    ori_fp = open(r'C:\Users\the__\PycharmProjects\tools\reptile\document_original\original_data.txt', 'r')
+    url_json = json.load(ori_fp)
+    ori_fp.close()
+    video_list = url_json['data']['videoList']
+    ori_dir = 'C:\\Users\\the__\\PycharmProjects\\tools\\reptile\\document_original'
+    if not os.path.exists(ori_dir):
+        os.makedirs(ori_dir)
+    for i in range(len(video_list)):
+        url_srt_cn = video_list[i]['subList'][0]['subUrl']
+        url_srt_en = video_list[i]['subList'][1]['subUrl']
+        srt_filename_cn = 'session' + str(i) + '_cn.txt'
+        srt_filename_en = 'session' + str(i) + '_en.txt'
+        srt_dirname_cn = ori_dir + '\\' + srt_filename_cn
+        srt_dirname_en = ori_dir + '\\' + srt_filename_en
+        srt_content_cn = urllib2.urlopen(url_srt_cn)
+        srt_content_en = urllib2.urlopen(url_srt_en)
+
+        srt_cn = open(srt_dirname_cn, 'wb')
+        srt_cn.write(srt_content_cn.read())
+        srt_cn.close()
+        print '第', i+1, '课的中文字幕已写入document_original文件夹下的', str(srt_filename_cn),'文件！'
+        srt_en = open(srt_dirname_en, 'wb')
+        srt_en.write(srt_content_en.read())
+        srt_en.close()
+        print '第', i+1, '课的英文字幕已写入document_original文件夹下的', str(srt_filename_en), '文件！'
 
 if __name__ == '__main__':
-    download()
+    download_srt()
---
+++ reptile/get_orignal_data.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+import json
+import os
+import requests
+
+
+def get_original_data():
+    url = 'http://c.open.163.com/mob/M6U6LS8CV/getMoviesForIpad.do'
+    original_data = requests.get(url=url)
+    ori_dir = 'C:\\Users\\the__\\PycharmProjects\\tools\\reptile\\document_original'
+    ori_file = ori_dir + '\original_data.txt'
+    if not os.path.exists(ori_dir):
+        os.makedirs(ori_dir)
+    with open(ori_file, 'wb') as fp:
+        json.dump(original_data.json(), fp)
+    return 0
+
+
+if __name__ == '__main__':
+    get_original_data()
